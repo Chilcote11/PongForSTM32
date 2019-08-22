61035e1c4971de078ab35b3a0dca0c3444c64198
Chilcote11 <50953394+Chilcote11@users.noreply.github.com>
2019-08-22 12:12:08 -0400

    create

---
+++ Debug/StdPeriph_Driver/src/stm32f0xx_wwdg.d
@@ -0,0 +1,86 @@
+StdPeriph_Driver/src/stm32f0xx_wwdg.o: \
+ ../StdPeriph_Driver/src/stm32f0xx_wwdg.c \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_wwdg.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/device/stm32f0xx.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/core/core_cm0.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/core/core_cmInstr.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/core/core_cmFunc.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/device/system_stm32f0xx.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_conf.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_cec.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_crc.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_comp.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_dac.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_dbgmcu.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_dma.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_exti.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_flash.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_gpio.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_syscfg.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_i2c.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_iwdg.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_pwr.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_rcc.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_rtc.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_spi.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_tim.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_usart.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_wwdg.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_misc.h \
+ U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_rcc.h
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_wwdg.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/device/stm32f0xx.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/core/core_cm0.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/core/core_cmInstr.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/core/core_cmFunc.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/device/system_stm32f0xx.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_conf.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_cec.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_crc.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_comp.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_dac.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_dbgmcu.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_dma.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_exti.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_flash.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_gpio.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_syscfg.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_i2c.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_iwdg.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_pwr.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_rcc.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_rtc.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_spi.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_tim.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_usart.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_wwdg.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_misc.h:
+
+U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc/stm32f0xx_rcc.h:
---
+++ Debug/src/subdir.mk
@@ -0,0 +1,53 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../src/micro_wait.s \
+../src/stdio.s 
+
+C_SRCS += \
+../src/main\ (6).c \
+../src/syscalls.c \
+../src/system_stm32f0xx.c 
+
+OBJS += \
+./src/main\ (6).o \
+./src/micro_wait.o \
+./src/stdio.o \
+./src/syscalls.o \
+./src/system_stm32f0xx.o 
+
+C_DEPS += \
+./src/main\ (6).d \
+./src/syscalls.d \
+./src/system_stm32f0xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/main\ (6).o: U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/src/main\ (6).c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -mfloat-abi=soft -DSTM32 -DSTM32F0 -DSTM32F051R8Tx -DSTM32F0DISCOVERY -DDEBUG -DSTM32F051 -DUSE_STDPERIPH_DRIVER -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/Utilities" -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc" -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/inc" -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/device" -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/core" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"src/main (6).d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/%.o: ../src/%.s
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Assembler'
+	@echo $(PWD)
+	arm-none-eabi-as -mcpu=cortex-m0 -mthumb -mfloat-abi=soft -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/Utilities" -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc" -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/inc" -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/device" -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/core" -g -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo $(PWD)
+	arm-none-eabi-gcc -mcpu=cortex-m0 -mthumb -mfloat-abi=soft -DSTM32 -DSTM32F0 -DSTM32F051R8Tx -DSTM32F0DISCOVERY -DDEBUG -DSTM32F051 -DUSE_STDPERIPH_DRIVER -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/Utilities" -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/StdPeriph_Driver/inc" -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/inc" -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/device" -I"U:/Personal/SystemWorkbench2.7/ECE362_MINI_PROJ/CMSIS/core" -O0 -g3 -Wall -fmessage-length=0 -ffunction-sections -c -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
